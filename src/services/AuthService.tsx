1f65565387c1ce55ed8a34950e320223dec6fb34
antoniorodriguestsx <antoniorodriguesmail98@gmail.com>
2026-01-16 10:40:21 -0300

    Concluído o serviço de cadastro de usuário

--- src/services/AuthService.tsx
+++ src/services/AuthService.tsx
@@ -38,3 +38,47 @@ export const authLogin = async (email: string, password: string) => {
     };
   }
 }
+
+export const authRegister = async (name: string, email: string, password: string) => {
+  try {
+    if (name.length < 1) {
+      return {
+        success: false,
+        error: "Name is required"
+      };
+    }
+
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+      return {
+        success: false,
+        error: "Invalid email address"
+      };
+    }
+
+    if (password.length < 5) {
+      return {
+        success: false,
+        error: "Short password"
+      };
+    }
+
+    const response = await axios({
+      method: 'post',
+      url: `${AUTH_API_URL}/register`,
+      data: { name, email, password }
+    });
+
+    return {
+      success: true,
+      data: response.data
+    }
+  }
+  catch (error) {
+    console.error(error);
+
+    return {
+      success: false,
+      error: null
+    };
+  }
+}
